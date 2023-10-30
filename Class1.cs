f47e68313cf987ac023cae7f1384f11f896629c7
Techy-Wu <Techy_Wu@outlook.com>
2023-10-30 18:46:06 +0800

    Initially Submit

---
+++ Class1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Windows.Storage;
+using System.IO;
+using Windows.Security.Cryptography;
+using System.Security.Cryptography;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.ViewManagement;
+using System.Net;
+using System.Threading;
+using Windows.UI.Xaml.Controls;
+
+namespace Let_s_Pick_UWP
+{
+    class GlobalClass
+    {
+        public static string current_version = "2.6.1";
+        public static string latest_version = "-1";
+
+        public static ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+        public static int RandMethod = 1;
+
+        public static string path = string.Empty;
+        public static ArrayList all_list = new ArrayList();
+
+        public static int current_list_id = 0;
+        public static ArrayList all_items = new ArrayList();
+        public static ArrayList rand_sequences = new ArrayList();
+
+        public static ArrayList s_name_list = new ArrayList();
+        public static ArrayList s_value_list = new ArrayList();
+        public static ArrayList s_type_list = new ArrayList();
+        public static ArrayList result_list = new ArrayList();
+        public static int auto_stop_tick;
+        public static int auto_stop_tick_min = 30;
+        public static int req_time_out = 5000;
+        public static bool auto_stop = true;
+        public static bool read_out = true;
+        public static bool toolbar_background_on = true;
+        public static bool randseed_visible = false;
+        public static bool pre_release = false;
+        public static bool variable_seed = false;
+
+        public static Brush white_br = new SolidColorBrush(Windows.UI.Colors.WhiteSmoke);
+
+        private UISettings uisetting = new UISettings();
+
+        public static int GenerateRandomNumber()
+        {
+            // Generate a random number.
+            int random = (int)(CryptographicBuffer.GenerateRandomNumber() % int.MaxValue);
+            return random;
+        }
+
+        public static string Md5(string str)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            string t2 = BitConverter.ToString(md5.ComputeHash(UTF8Encoding.Default.GetBytes(str)), 4, 8);
+            t2 = t2.Replace("-", "");
+            return t2;
+        }
+
+        /// <summary>
+        /// 获取一个颜色的人眼感知亮度，并以 0~1 之间的小数表示。
+        /// </summary>
+        private static double GetGrayLevel(Color color)
+        {
+            return (0.299 * color.R + 0.587 * color.G + 0.114 * color.B) / 255;
+        }
+
+        public static bool GetReverseForegroundColor()     //false-black   true-white
+        {
+            if(GetGrayLevel(new UISettings().GetColorValue(UIColorType.Accent)) > 0.5)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static async void GetLatestVersion()
+        {
+            string url = string.Empty;
+            WebRequest myWebRequest = WebRequest.Create("https://github.com/Techy-Wu/Let-s_Pick_UWP/releases/latest");
+            myWebRequest.Timeout = req_time_out;
+            try
+            {
+                WebResponse response = await myWebRequest.GetResponseAsync();
+                url = response.ResponseUri.ToString();
+                latest_version = url.Replace("https://github.com/Techy-Wu/Let-s_Pick_UWP/releases/tag/", "");
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+        }
+
+        public static async Task GetSequencesFromRandomORG(int min, int max)
+        {
+            string url = "https://www.random.org/sequences/?min=" + min + "&max=" + max + "&col=1&format=plain&rnd=new";
+            string htmlStr = "";
+
+            for(int t = 1; t <= 3; t++)
+            {
+                try
+                {
+                    WebRequest request;          //实例化WebRequest对象
+                    request = WebRequest.Create(url);
+                    WebResponse response;      //创建WebResponse对象
+                    response = await request.GetResponseAsync();
+                    //Stream datastream = response.GetResponseStream();       //创建流对象
+                    Stream datastream = response.GetResponseStream();
+                    Encoding ec = Encoding.Default;
+                    ec = Encoding.UTF8;
+                    StreamReader reader = new StreamReader(datastream, ec);
+                    htmlStr = reader.ReadToEnd();                  //读取网页内容  
+                    reader.Close();
+                    datastream.Close();
+                    response.Close();
+
+                    int j = 0;
+                    using (StringReader sr = new StringReader(htmlStr))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            GlobalClass.rand_sequences.Add(line.ToString());
+                        }
+                    }
+
+                    t = 4;
+                }
+                catch (Exception ex)
+                {
+                    GlobalClass.rand_sequences.Add("Error");
+                    int j = 0;
+                    using (StringReader sr = new StringReader(ex.Message))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            GlobalClass.rand_sequences.Add(line.ToString());
+                        }
+                    }
+                    GlobalClass.rand_sequences.Add("Error");
+
+                    ContentDialog dialog = new ContentDialog();
+                    dialog.Title = "Request Error";
+                    string error_info = string.Empty;
+                    foreach (var item in GlobalClass.rand_sequences)
+                    {
+                        if (item.ToString() == "Error")
+                            break;
+
+                        error_info += (string)item;
+                        error_info += "\n";
+                    }
+                    dialog.Content = error_info;
+                    dialog.PrimaryButtonText = "Ok";
+                    await dialog.ShowAsync();
+                }
+            }
+        }
+    }
+}
---
+++ SettingPage.xaml.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Data;
+using Windows.Storage;
+using Windows.Globalization;
+using Windows.ApplicationModel.Core;
+using System.Collections;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace Let_s_Pick_UWP
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class SettingPage : Page
+    {
+        Windows.Storage.Pickers.FileOpenPicker open_Picker = new Windows.Storage.Pickers.FileOpenPicker();
+
+        public SettingPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            open_Picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            open_Picker.FileTypeFilter.Add(".lpst");
+
+            AutoStop.IsOn = GlobalClass.auto_stop;
+            ToolBar_Background_Switch.IsOn = GlobalClass.toolbar_background_on;
+            RandSeed_Visible_Switch.IsOn = GlobalClass.randseed_visible;
+            TickSet.Text = GlobalClass.auto_stop_tick.ToString();
+            LanguageSelector.ItemsSource = ApplicationLanguages.ManifestLanguages;
+            LanguageSelector.SelectedItem = ApplicationLanguages.PrimaryLanguageOverride;
+            RandMethod_Selector.SelectedIndex = GlobalClass.RandMethod;
+            Pre_Caculate_Switch.IsOn = GlobalClass.pre_release;
+            Variable_Seed_Switch.IsOn = GlobalClass.variable_seed;
+            UpdateSearchTimeout.Text = GlobalClass.req_time_out.ToString();
+
+            try
+            {
+                for (int i = 0; i < GlobalClass.all_list.Count; i++)
+                {
+                    ArrayList temp = new ArrayList();
+                    temp = (ArrayList)GlobalClass.all_list[i];
+                    CurrentListSelector.Items.Add(temp[0].ToString().Remove(0, 1));
+                }
+            }
+            catch (Exception)
+            {
+            }
+            CurrentListSelector.SelectedIndex = GlobalClass.current_list_id;
+
+
+            if(ApplicationLanguages.PrimaryLanguageOverride == string.Empty)
+            {
+                LanguageSelector.Text = "(Auto)";
+            }
+        }
+
+        private void LanguageSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(ApplicationLanguages.PrimaryLanguageOverride != LanguageSelector.SelectedItem.ToString())
+            {
+                RestartInfoStack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                RestartInfoStack.Visibility = Visibility.Collapsed;
+            }
+            ApplicationLanguages.PrimaryLanguageOverride = LanguageSelector.SelectedItem.ToString();
+        }
+
+        private void Page_LosingFocus(UIElement sender, LosingFocusEventArgs args)
+        {
+            try
+            {
+                try
+                {
+                    GlobalClass.auto_stop_tick = Convert.ToInt32(TickSet.Text.ToString());
+                }
+                catch (Exception)
+                {
+                    TickSet.Text = GlobalClass.auto_stop_tick_min.ToString();
+                }
+
+                GlobalClass.current_list_id = CurrentListSelector.SelectedIndex == -1 ? 0 : CurrentListSelector.SelectedIndex;
+                GlobalClass.RandMethod = RandMethod_Selector.SelectedIndex;
+                GlobalClass.pre_release = Pre_Caculate_Switch.IsOn;
+                GlobalClass.variable_seed = Variable_Seed_Switch.IsOn;
+
+                GlobalClass.localSettings.Values["Auto_Stop_Tick"] = GlobalClass.auto_stop_tick;
+                GlobalClass.localSettings.Values["ToolBar_Background_State"] = ToolBar_Background_Switch.IsOn;
+                GlobalClass.localSettings.Values["Current_List_id"] = GlobalClass.current_list_id;
+                GlobalClass.localSettings.Values["RandSeed_Visible_State"] = RandSeed_Visible_Switch.IsOn;
+                GlobalClass.localSettings.Values["RandMethod"] = RandMethod_Selector.SelectedIndex;
+                GlobalClass.localSettings.Values["Pre_Release"] = Pre_Caculate_Switch.IsOn;
+                GlobalClass.localSettings.Values["Variable_Seed"] = Variable_Seed_Switch.IsOn;
+                GlobalClass.localSettings.Values["UpdateRequestTimeout"] = GlobalClass.req_time_out;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void TickSet_LosingFocus(UIElement sender, LosingFocusEventArgs args)
+        {
+            try
+            {
+                GlobalClass.auto_stop_tick = Convert.ToInt32(TickSet.Text.ToString());
+            }
+            catch (Exception)
+            {
+                TickSet.Text = GlobalClass.auto_stop_tick_min.ToString();
+            }
+            if(GlobalClass.auto_stop_tick < GlobalClass.auto_stop_tick_min)
+            {
+                TickSet.Text = GlobalClass.auto_stop_tick_min.ToString();
+                GlobalClass.auto_stop_tick = GlobalClass.auto_stop_tick_min;
+            }
+        }
+
+        private void AutoStop_Toggled(object sender, RoutedEventArgs e)
+        {
+            GlobalClass.auto_stop = AutoStop.IsOn;
+        }
+
+        private async void Restart_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            await CoreApplication.RequestRestartAsync(string.Empty);
+        }
+
+        private async void ImportSettingsButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Windows.Storage.StorageFile file = await open_Picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                string fname = file.Path;
+                File.WriteAllText(GlobalClass.path + "\\slist.lpst", await Windows.Storage.FileIO.ReadTextAsync(file));
+            }
+        }
+
+        private void ToolBar_Background_Switch_Toggled(object sender, RoutedEventArgs e)
+        {
+            GlobalClass.toolbar_background_on = ToolBar_Background_Switch.IsOn;
+        }
+
+        private void RandSeed_Visible_Switch_Toggled(object sender, RoutedEventArgs e)
+        {
+            GlobalClass.randseed_visible = RandSeed_Visible_Switch.IsOn;
+        }
+
+        private void RandMethod_Selector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                int index = RandMethod_Selector.SelectedIndex;
+                Pre_Caculate_Switch.Visibility = Visibility.Visible;
+                Variable_Seed_Switch.Visibility = Visibility.Visible;
+                if (index == 0 || index == 1 || index == 4 || index == 5)
+                {
+                    Pseudorandom_Notice.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    Pseudorandom_Notice.Visibility = Visibility.Collapsed;
+                    if (index == 6)
+                    {
+                        Pre_Caculate_Switch.Visibility = Visibility.Collapsed;
+                        Variable_Seed_Switch.Visibility = Visibility.Collapsed;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void UpdateSearchTimeout_LosingFocus(UIElement sender, LosingFocusEventArgs args)
+        {
+            try
+            {
+                GlobalClass.req_time_out = Convert.ToInt32(UpdateSearchTimeout.Text.ToString());
+            }
+            catch (Exception)
+            {
+                UpdateSearchTimeout.Text = GlobalClass.req_time_out.ToString();
+            }
+            if (GlobalClass.req_time_out < 1500)
+            {
+                GlobalClass.req_time_out = 1500;
+                UpdateSearchTimeout.Text = GlobalClass.req_time_out.ToString();
+            }
+        }
+    }
+}
